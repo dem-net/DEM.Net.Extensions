a16c39544e0ac3962055d6d0d04d81ed32d117fb
Xavier Fischer <xavier@fischer.ninja>
2020-09-26 00:26:32 +0200

    refacto export as separate files

--- DEM.Net.Extension.VisualTopo/VisualTopoService.cs
+++ DEM.Net.Extension.VisualTopo/VisualTopoService.cs
@@ -23,21 +23,15 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using ClosedXML.Excel;
 using DEM.Net.Core;
 using DEM.Net.Core.Graph;
-using DEM.Net.Core.Services.Lab;
-using DEM.Net.glTF.SharpglTF;
 using Microsoft.Extensions.Logging;
-using SixLabors.ImageSharp.ColorSpaces;
-using SixLabors.ImageSharp.ColorSpaces.Conversion;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Numerics;
 using System.Text;
 
@@ -171,146 +165,11 @@ namespace DEM.Net.Extension.VisualTopo
 
         public MemoryStream ExportToCsv(VisualTopoModel model)
         {
-            MemoryStream ms = new MemoryStream();
-            using (StreamWriter sw = new StreamWriter(ms))
-            {
-                sw.WriteLine(string.Join("\t", "Entree", "Sortie", "Longueur", "Cap", "Pente", "Gauche", "Droite", "Haut", "Bas", "X", "Y", "Z", "Distance", "Profondeur", "Commentaire"));
-
-                foreach (var set in model.Sets)
-                {
-                    sw.WriteLine(string.Join("\t", "Entree", "Sortie", "Longueur", "Cap", "Pente", set.Color.ToRgbString(), "", "", "", "", "", "", "", "", set.Name));
-
-                    foreach (var data in set.Data)
-                    {
-                        sw.WriteLine(string.Join("\t", data.Entree, data.Sortie, data.Longueur, data.Cap, data.Pente
-                            , data.CutSection.left, data.CutSection.right, data.CutSection.up, data.CutSection.down
-                            , data.VectorLocal.X, data.VectorLocal.Y, data.VectorLocal.Z
-                            , data.DistanceFromEntry, data.Depth, data.Comment));
-                    }
-                }
-
-            }
-            return ms;
+            return model.ExportToCsv();
         }
         public MemoryStream ExportToExcel(VisualTopoModel model, bool autoFitColumns = true)
         {
-            using (TimeSpanBlock timer = new TimeSpanBlock("VisualTopo Excel export", _logger))
-            using (var wb = new XLWorkbook(XLEventTracking.Disabled))
-            {
-                var ws = wb.Worksheets.Add($"Cavité entrée Z={model.EntryPoint.Elevation:N1} m");
-
-                int ro = 1;
-                int co = 0;
-                ws.Cell(ro, ++co).Value = "Pt Départ";
-                ws.Cell(ro, ++co).Value = "Pt Arrivée";
-                ws.Cell(ro, ++co).Value = "Longueur";
-                ws.Cell(ro, ++co).Value = "Cap";
-                ws.Cell(ro, ++co).Value = "Pente";
-                ws.Cell(ro, ++co).Value = "Gauche";
-                ws.Cell(ro, ++co).Value = "Droite";
-                ws.Cell(ro, ++co).Value = "Haut";
-                ws.Cell(ro, ++co).Value = "Bas";
-                ws.Cell(ro, ++co).Value = "X";
-                ws.Cell(ro, ++co).Value = "Y";
-                ws.Cell(ro, ++co).Value = "Z";
-                ws.Cell(ro, ++co).Value = "Latitude";
-                ws.Cell(ro, ++co).Value = "Longitude";
-                ws.Cell(ro, ++co).Value = "X (Lambert 93)";
-                ws.Cell(ro, ++co).Value = "Y (Lambert 93)";
-                ws.Cell(ro, ++co).Value = "Distance";
-                ws.Cell(ro, ++co).Value = "Profondeur relative entrée";
-                ws.Cell(ro, ++co).Value = "Altitude terrain";
-                ws.Cell(ro, ++co).Value = "Profondeur réelle";
-                ws.Cell(ro, ++co).Value = "Commentaire";
-
-                var row = ws.Row(ro);
-                row.Style.Fill.BackgroundColor = XLColor.GreenYellow;
-                row.Style.Font.Bold = true;
-
-                foreach (var set in model.Sets)
-                {
-                    ro++;
-                    co = 0;
-                    ws.Cell(ro, ++co).Value = "Section";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "Pente";
-                    ws.Cell(ro, ++co).Value = set.Color.ToRgbString();
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = "";
-                    ws.Cell(ro, ++co).Value = set.Name;
-
-                    var setRow = ws.Row(ro);
-                    setRow.Style.Fill.BackgroundColor = XLColor.GreenYellow;
-                    setRow.Style.Font.Bold = true;
-
-                    foreach (var data in set.Data)
-                    {
-                        ro++;
-                        co = 0;
-                        ws.Cell(ro, ++co).Value = string.Concat("'", data.Entree);
-                        ws.Cell(ro, ++co).Value = string.Concat("'", data.Sortie);
-                        ws.Cell(ro, ++co).Value = data.Longueur; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.Cap; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.Pente; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.CutSection.left; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.CutSection.right; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.CutSection.up; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.CutSection.down; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.VectorLocal.X; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.VectorLocal.Y; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.VectorLocal.Z; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        // Latitude, Long, X,Y
-                        ws.Cell(ro, ++co).Value = data.GeoPointGlobal_DEMProjection?.Latitude;//  ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.GeoPointGlobal_DEMProjection?.Longitude;  //ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.GeoPointGlobal_ProjectedCoords?.Longitude; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.GeoPointGlobal_ProjectedCoords?.Latitude; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-
-                        ws.Cell(ro, ++co).Value = data.DistanceFromEntry; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.VectorLocal.Z; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        ws.Cell(ro, ++co).Value = data.TerrainElevationAbove; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-                        var formula = string.Format(CultureInfo.InvariantCulture, "=RC[-1]-{0:N2}-RC[-2]", model.EntryPoint.Elevation ?? 0);
-                        ws.Cell(ro, ++co).FormulaR1C1 = formula; ws.Cell(ro, co).Style.NumberFormat.NumberFormatId = 2;
-
-                        // comment row (red for disconnected node)
-                        if (data.IsDisconnected)
-                        {
-                            var disconnectedRow = ws.Row(ro);
-                            setRow.Style.Fill.BackgroundColor = XLColor.Red;
-                            disconnectedRow.Style.Fill.BackgroundColor = XLColor.Red;
-                            ws.Cell(ro, ++co).Value = string.Concat("[DISCONNECTED] ", data.Comment);
-                        }
-                        else
-                        {
-                            ws.Cell(ro, ++co).Value = data.Comment;
-                        }
-
-                    }
-                }
-
-                if (autoFitColumns) ws.Columns().AdjustToContents();
-
-                wb.CalculateMode = XLCalculateMode.Auto;
-
-                MemoryStream ms = new MemoryStream();
-                wb.SaveAs(ms, validate: true, evaluateFormulae: true);
-                ms.Seek(0, SeekOrigin.Begin);
-
-                return ms;
-            }
+            return model.ExportToExcel(autoFitColumns);
         }
 
         // Elevations
